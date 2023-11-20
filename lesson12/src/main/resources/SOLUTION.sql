87e59ab75591554fe96d557c7f9b9a091460e5a4
Welkus <vdelkus@gmail.com>
2023-11-20 23:07:48 +0200

    Test 12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,7 +1,5 @@
-DELETE FROM mark WHERE student_id IN (SELECT DISTINCT s.id FROM STUDENT s JOIN mark m ON s.id = m.student_id JOIN subject ss ON m.subject_id = ss.id WHERE ss.grade > 4);
-DELETE FROM payment where student_id in (select distinct s.id from student s join mark m on s.id = m.student_id join subject ss on m.subject_id = ss.id where ss.grade > 4);
-DELETE FROM student where id in (select distinct s.id from student s join mark m on s.id = m.student_id join subject ss on m.subject_id = ss.id where ss.grade > 4);
-DELETE FROM MARK WHERE student_id IN (SELECT DISTINCT s.id FROM STUDENT s JOIN MARK m ON s.id = m.student_id WHERE m.mark < 4);
-UPDATE payment set type_id = null where type_id = 1;
-DELETE FROM paymenttype where id = 1;
-delete from mark where mark < 7;
+
+delete from student where id in (select student_id from mark where subject_id in (select id from subject where grade > 4));
+delete from student where id in (select student_id from mark group by student_id having min(mark) < 4);
+delete from paymenttype where name = 'DAILY';
+delete FROM mark where mark < 7;
