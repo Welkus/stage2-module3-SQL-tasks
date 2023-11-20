4f62e1199b5b44466abff3ad3fdf0bf30eebaecd
Welkus <vdelkus@gmail.com>
2023-11-20 20:08:11 +0200

    Test 11

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM mark WHERE student_id IN (SELECT DISTINCT s.id FROM STUDENT s JOIN mark m ON s.id = m.student_id JOIN subje ss ON m.subject_id = ss.id WHERE ss.grade > 4);
+DELETE FROM mark WHERE student_id IN (SELECT DISTINCT s.id FROM STUDENT s JOIN mark m ON s.id = m.student_id JOIN subject ss ON m.subject_id = ss.id WHERE ss.grade > 4);
 DELETE FROM payment where student_id in (select distinct s.id from student s join mark m on s.id = m.student_id join subject ss on m.subject_id = ss.id where ss.grade > 4);
 DELETE FROM student where id in (select distinct s.id from student s join mark m on s.id = m.student_id join subject ss on m.subject_id = ss.id where ss.grade > 4);
 DELETE FROM MARK WHERE student_id IN (SELECT DISTINCT s.id FROM STUDENT s JOIN MARK m ON s.id = m.student_id WHERE m.mark < 4);
