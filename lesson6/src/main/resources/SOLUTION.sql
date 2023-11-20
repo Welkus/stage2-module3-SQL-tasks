a5b4aac62ab127aa0394bff9cef4df19ae3a2c3b
Welkus <vdelkus@gmail.com>
2023-11-20 17:54:34 +0200

    Test 9

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT * FROM PAYMENT WHERE type_id in (select id from paymenttype where name = 'MONTHLY' );
-SELECT * FROM MARK WHERE subject_id in (select id from subject where name = 'Art')
+SELECT * FROM MARK WHERE subject_id in (select id from subject where name = 'Art');
 SELECT * FROM student where id in (select student_id from payment where type_id in (select id from paymenttype where name = 'WEEKLY'));
 SELECT * FROM student where id in (select student_id from mark where subject_id in (select id from subject where name = 'Math'));
