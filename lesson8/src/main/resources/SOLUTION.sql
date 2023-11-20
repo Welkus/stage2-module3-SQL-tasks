f2ac9c9f85964db832f803f43f9b35e948ff4bc8
Welkus <vdelkus@gmail.com>
2023-11-20 14:54:27 +0200

    Test 6

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT birthday FROM STUDENT order by birthday desc LIMIT 1;
 SELECT payment_date FROM PAYMENT order by payment_date limit 1;
-SELECT AVG(mark) FROM MARK WHERE subject_id = 6;
+SELECT AVG(mark) FROM MARK WHERE subject_id in (select id from subject where name = 'Math');
 SELECT MIN(amount) FROM PAYMENT WHERE type_id = 2;
